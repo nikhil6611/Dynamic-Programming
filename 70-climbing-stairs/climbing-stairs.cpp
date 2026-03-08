f77f190f7ea40a92f712d802d2587502fcfb0b08
nikhil6611 <nikhiltiwari6611@gmail.com>
2026-03-08 14:26:59 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (70.26%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,16 +1,18 @@
-//Using tabulation method
+//Using tabulation method+space optimization
 class Solution {
 public:
    
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-       dp[0]={1};
-       int ss=0;
-       for(int i=1;i<=n;i++){
-        int fs=dp[i-1];
-       if(i>1)  ss=dp[i-2];
-       dp[i]=fs+ss;
+        int prev=1;
+        int prev2=0;
+        for(int i=1;i<=n;i++){
+            int curr= prev+prev2;
+            prev2=prev;
+            prev=curr;
+           
+        }
+        return prev;
        }
-       return dp[n];
-    }
+       
+    
 };
